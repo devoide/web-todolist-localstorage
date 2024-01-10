6b5182a72c4d962737522d51afd7a39d8bd3f16c
moccil-bzz <moccil@bzz.com>
2024-01-10 13:54:48 +0100

    for now done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", load_content)
 function load_content(){
     fetchItems()
     document.getElementById("done").addEventListener("click", doneButtonClickHandler);
+    document.getElementById("inputText").focus();
 }
 
 function fetchItems() {
@@ -10,63 +11,77 @@ function fetchItems() {
     while (parentdiv.firstChild) {
         parentdiv.removeChild(parentdiv.firstChild);
     }
-    Object.keys(localStorage).forEach(key => {
-        if (key.endsWith('-index')) {
-            try {
-                const item = JSON.parse(localStorage.getItem(key))
-                const inp = document.createElement('input');
-                const lab = document.createElement('label');
-                const br = document.createElement('br');
-                const cancel = document.createElement('span');
-                const div = document.createElement('div');
-                inp.classList.add("list_item");
-                inp.type = "checkbox";
-                lab.innerText = item[0];
-                lab.classList.add("listItem")
-                cancel.classList.add("cancelbut");
-                cancel.innerHTML = '  &times;   ';
-                div.id = 'div-' + item[0] + '-' + key;
-                cancel.id = 'cancel-' + item[0] + '-' + key;
-                div.classList.add("item-div");
-                //console.log(item.checked)
-                inp.checked = item[1] !== 0;
-                div.appendChild(inp);
-                div.appendChild(lab);
-                div.appendChild(cancel);
-                div.appendChild(br);
-                parentdiv.appendChild(div);
-                cancel.addEventListener("click", () => {
-                    deleter(key);
-                });
-                inp.addEventListener("change", () => {
-                    changer(key)
-                });
-            } catch (e) {
-                console.error("Error during reading value:", e);
-            }
-        }
-    });
+    try {
+        const items = JSON.parse(localStorage.getItem('todoItems'))
+        items.forEach(item => {
+            const inp = document.createElement('input');
+            const lab = document.createElement('label');
+            const br = document.createElement('br');
+            const cancel = document.createElement('span');
+            const div = document.createElement('div');
+            inp.classList.add("list_item");
+            inp.type = "checkbox";
+            lab.innerText = item.content;
+            lab.classList.add("listItem")
+            cancel.classList.add("cancelbut");
+            cancel.innerHTML = '  &times;   ';
+            div.id = 'div-' + item.id;
+            cancel.id = 'cancel-' + item.id;
+            div.classList.add("item-div");
+            //console.log(item.checked)
+            inp.checked = item.completed !== 0;
+            div.appendChild(inp);
+            div.appendChild(lab);
+            div.appendChild(cancel);
+            div.appendChild(br);
+            parentdiv.appendChild(div);
+            cancel.addEventListener("click", () => {
+                deleter(item.id);
+            });
+            inp.addEventListener("change", () => {
+                changer(item.id)
+            });
+        });
+
+    } catch (e) {
+        console.error("Error during reading value:", e);
+    }
 }
 
 function doneButtonClickHandler() {
     let listItem = document.getElementById("inputText").value;
     console.log(listItem === '')
     if (listItem !== '') {
-        const value = [listItem, 0]
-        localStorage.setItem(Date.now().toString() + '-index', JSON.stringify(value))
+        const storedArrayJSON = localStorage.getItem('todoItems');
+        const storedArray = storedArrayJSON ? JSON.parse(storedArrayJSON) : [];
+        const new_value = {id: Date.now().toString(), content: listItem, completed: 0}
+        storedArray.push(new_value)
+        localStorage.setItem('todoItems', JSON.stringify(storedArray))
         fetchItems()
     }
 }
 
-function deleter(key) {
-    localStorage.removeItem(key)
-    fetchItems()
+function deleter(id) {
+    const items = JSON.parse(localStorage.getItem('todoItems'));
+    const arrayIndex = items.findIndex(items => items.id === id);
+    if (arrayIndex !== -1) {
+        items.splice(arrayIndex, 1);
+        localStorage.setItem('todoItems', JSON.stringify(items));
+        fetchItems();
+    } else {
+        console.log('Item not found with id:', id);
+    }
 }
 
-function changer(key) {
-    const value = JSON.parse(localStorage.getItem(key))
-    let checked = 1 - value[1]
-    const new_value = [value[0], checked]
-    localStorage.setItem(key, JSON.stringify(new_value))
+function changer(id) {
+    const items = JSON.parse(localStorage.getItem('todoItems'));
+    const arrayIndex = items.findIndex(items => items.id === id);
+    if (arrayIndex !== -1) {
+        items[arrayIndex].completed = 1 - items[arrayIndex].completed
+        localStorage.setItem('todoItems', JSON.stringify(items));
+        fetchItems();
+    } else {
+        console.log('Item not found with id:', id);
+    }
     fetchItems()
 }
