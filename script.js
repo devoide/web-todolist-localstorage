66e7be4d2899c3638a568dfd00e1bb814fbdae4b
moccil-bzz <moccil@bzz.com>
2024-01-10 10:35:33 +0100

    for now done

--- script.js
+++ script.js
@@ -12,34 +12,38 @@ function fetchItems() {
     }
     Object.keys(localStorage).forEach(key => {
         if (key.endsWith('-index')) {
-            let item = localStorage.getItem(key)
-            const inp = document.createElement('input');
-            const lab = document.createElement('label');
-            const br = document.createElement('br');
-            const cancel = document.createElement('span');
-            const div = document.createElement('div');
-            inp.classList.add("list_item");
-            inp.type = "checkbox";
-            lab.innerText = item;
-            lab.classList.add("listItem")
-            cancel.classList.add("cancelbut");
-            cancel.innerHTML = '  &times;   ';
-            div.id = 'div-' + item + '-' + key;
-            cancel.id = 'cancel-' + item + '-' + key;
-            div.classList.add("item-div");
-            //console.log(item.checked)
-            //inp.checked = item.checked !== 0;
-            div.appendChild(inp);
-            div.appendChild(lab);
-            div.appendChild(cancel);
-            div.appendChild(br);
-            parentdiv.appendChild(div);
-            cancel.addEventListener("click", () => {
-                deleter(key);
-            });
-            inp.addEventListener("change", () => {
-                changer(key)
-            });
+            try {
+                const item = JSON.parse(localStorage.getItem(key))
+                const inp = document.createElement('input');
+                const lab = document.createElement('label');
+                const br = document.createElement('br');
+                const cancel = document.createElement('span');
+                const div = document.createElement('div');
+                inp.classList.add("list_item");
+                inp.type = "checkbox";
+                lab.innerText = item[0];
+                lab.classList.add("listItem")
+                cancel.classList.add("cancelbut");
+                cancel.innerHTML = '  &times;   ';
+                div.id = 'div-' + item[0] + '-' + key;
+                cancel.id = 'cancel-' + item[0] + '-' + key;
+                div.classList.add("item-div");
+                //console.log(item.checked)
+                inp.checked = item[1] !== 0;
+                div.appendChild(inp);
+                div.appendChild(lab);
+                div.appendChild(cancel);
+                div.appendChild(br);
+                parentdiv.appendChild(div);
+                cancel.addEventListener("click", () => {
+                    deleter(key);
+                });
+                inp.addEventListener("change", () => {
+                    changer(key)
+                });
+            } catch (e) {
+                console.error("Error during reading value:", e);
+            }
         }
     });
 }
@@ -48,16 +52,21 @@ function doneButtonClickHandler() {
     let listItem = document.getElementById("inputText").value;
     console.log(listItem === '')
     if (listItem !== '') {
-        localStorage.setItem(Date.now().toString() + '-index', listItem)
+        const value = [listItem, 0]
+        localStorage.setItem(Date.now().toString() + '-index', JSON.stringify(value))
         fetchItems()
     }
 }
 
-function deleter(index) {
-    localStorage.removeItem(index)
+function deleter(key) {
+    localStorage.removeItem(key)
     fetchItems()
 }
 
-function changer(index) {
-    console.log("sum")
+function changer(key) {
+    const value = JSON.parse(localStorage.getItem(key))
+    let checked = 1 - value[1]
+    const new_value = [value[0], checked]
+    localStorage.setItem(key, JSON.stringify(new_value))
+    fetchItems()
 }
